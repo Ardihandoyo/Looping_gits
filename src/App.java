a9d7abe85b0ff38ee4e80c1605e5aac0adcc847b
Ardihandoyo <ardolnhm@gmail.com>
2021-09-03 18:58:33 +0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class buah {
+    int berapa;
+    String nama;
+    public void display(){
+        System.out.println(" Fruit Store");
+    }
+}
+
+class manis extends buah {
+    @Override
+    public void display(){
+        System.out.println("Buah Manis : ");
+        String [] arrBuah = {"Mangga","Manggis","Naga"};
+        for (int i = 0; i < arrBuah.length; i++){
+            System.out.println(arrBuah[i]);
+        } 
+        
+    }    
+}
+
+class asam extends buah {
+    @Override
+    public void display(){
+        System.out.println("Buah Asam :");
+        String [] arrAsam = {"Jeruk","Nanas","Lemon"};
+        for (int i = 0; i < arrAsam.length; i++){
+            System.out.println(arrAsam[i]);
+        } 
+
+    }
+}
+
+
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner inputan = new Scanner(System.in);
+        System.out.print("Buah Siapa = ");
+        String name = inputan.next();
+        System.out.print(name);
+        manis h1 = new manis();
+        asam h2 = new asam();
+        buah h3 = new buah();
+        h3.display();
+        System.out.println("============================================\nTersedia 2 pilihan Buah :");
+        h1.display();
+        h2.display();
+    }
+}
